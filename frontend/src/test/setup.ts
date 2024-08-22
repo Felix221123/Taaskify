0c59356a2463752519dcf72c877f59b93c623686
Felix221123 <felixbaah47@gmail.com>
2024-08-22 22:16:20 +0100

    Fix : githut actions was failing due to the fact that i was calling an endpoint api in a usecontext file in the useUserContext

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+import { vi } from "vitest"
+
+vi.mock('../packages/FetchManager/fetchData', () => ({
+  FetchData: vi.fn(() => Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ id: 1, name: 'Test User' }),
+  })),
+}));
