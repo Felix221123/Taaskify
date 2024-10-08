70d69c9752d46202f0e10445f0525ce23a107a02
Felix221123 <felixbaah47@gmail.com>
2024-10-08 15:43:59 +0100

    Fix : made some cors changes for more security

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,13 +1,21 @@
 import { Server } from 'socket.io';
 import { Server as HttpServer } from 'http'; // To differentiate between Socket.IO Server and HTTP Server
-import config from './Config/config';
 
 let io: Server | null = null;
 
 export const initSocket = (server: HttpServer) => {
+
+  // allowed origins
   io = new Server(server, {
     cors: {
-      origin: [config.server.base_url],
+      origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        const allowedOrigins = ['https://taaskify.com/','http://localhost:5173'];
+        if (origin && allowedOrigins.includes(origin)) {
+          callback(null, true); // Allow the request
+        } else {
+          callback(new Error('Not allowed by CORS')); // Block the request
+        }
+      },
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     },
